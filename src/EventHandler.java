9482499f5f68e581d4950dfee5fe66adf697b573
Ethan Cotterell <e.cotterell@lancaster.ac.uk>
2019-01-11 18:40:36 +0000

    Added JavaDoc comments

--- src/EventHandler.java
+++ src/EventHandler.java
@@ -1,5 +1,12 @@
+/**
+ * Custom handler for all events that occur and can be bound to
+ * specific objects
+ */
 public class EventHandler
 {
+    /**
+     * By default does nothing. Meant to be overridden
+     */
     public void run()
     {
         ;
