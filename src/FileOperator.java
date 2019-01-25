c078632b61853d1e250c1675d0c4377cbdaa4525
Ethan Cotterell <e.cotterell@lancaster.ac.uk>
2019-01-25 11:09:52 +0000

    Created test cases for FileOperator

--- src/FileOperator.java
+++ src/FileOperator.java
@@ -1,35 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
 
 public class FileOperator
 {
     private File file;
+    private boolean available = false;
+    private Exception exception = null;
+
     public FileOperator(String path)
     {
-        try{
+        try {
             file = new File(path);
-        } catch(Exception e){
-            System.out.println("ERROR- Could not read file");
+            available = true;
+        } catch (Exception e) {
+//            System.out.println("ERROR- Could not read file: " + path);
             e.printStackTrace();
+            exception = e;
         }
     }
 
     /**
      * Reads entire file
-     * @return (ArrayList<String>) Contents of file. Element per row
+     *
+     * @return (ArrayList < String >) Contents of file. Element per row
      */
     public ArrayList<String> readToList()
     {
-        StringBuilder builder = new StringBuilder();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file.getCanonicalFile()));
             String line;
-            ArrayList<String> contents = new ArrayList();
-            while ((line = reader.readLine()) != null){ //read current row into line
+            ArrayList<String> contents = new ArrayList<>();
+            while ((line = reader.readLine()) != null) { //read current row into line
                 contents.add(line); //add line into ArrayList contents
             }
             return contents;
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
@@ -37,7 +45,8 @@ public class FileOperator
 
     /**
      * Reads row of file
-     * @param (int) Row
+     *
+     * @param row (int) Row
      * @return (String[]) Returns array of elements in row
      */
     public String[] readLine(int row)
@@ -45,72 +54,77 @@ public class FileOperator
         try {
             ArrayList<String> contents = readToList();
             return contents.get(row).split(",");
-        } catch(Exception e){
-            System.out.println("Error- Could not read line. Check parameters");
+        } catch (Exception e) {
+//            System.err.println("Error- Could not read line. Check parameters");
             return null;
         }
     }
 
     /**
      * Reads single element in file
-     * @param (int) Row
-     * @param (int) Column
+     *
+     * @param row (int) Row
+     * @param col (int) Column
      * @return (String) Returns element in row
      */
     public String readElement(int row, int col)
     {
-        try{
+        try {
             return readLine(row)[col];
-        } catch(Exception e){
-            System.out.println("Error- Could not read element. Check parameters");
+        } catch (Exception e) {
+//            System.err.println("Error- Could not read element. Check parameters");
             return null;
         }
     }
 
     /**
      * Swaps specified element, by row and column, to provided String
-     * @param (String) String to swap element with
-     * @param (int) Row
-     * @param (int) Column
+     *
+     * @param add (String) String to swap element with
+     * @param row (int) Row
+     * @param col (int) Column
      * @return (boolean) Returns true if insert was successful, false if not
      */
     public boolean insert(String add, int row, int col)
     {
-        try{
+        try {
             ArrayList<String> contents = readToList(); //ArrayList of Strings for each line
             String[] lineSplit = readLine(row); //String array of elements in specified line
             lineSplit[col] = add; //updates element
             String newL = String.join(",", lineSplit); //packs elements into one string, separated by commas
-            contents.set(row,newL); //replaces new line in ArrayList of rows
+            contents.set(row, newL); //replaces new line in ArrayList of rows
             clearFile();
             FileWriter appendFileWriter = new FileWriter(file, true); //Writes each row to file
-            for(int i=0; i<contents.size(); i++){
+            for (int i = 0; i < contents.size(); i++) {
                 appendFileWriter.append(contents.get(i));
-                if(i!=contents.size()-1) {appendFileWriter.append("\n");} //Don't want empty row at EOF 
+                if (i != contents.size() - 1) {
+                    appendFileWriter.append("\n");
+                } //Don't want empty row at EOF
             }
             appendFileWriter.flush();
             appendFileWriter.close();
             return true;
-        } catch(Exception e){
-            System.out.println("Insert Error");
+        } catch (Exception e) {
+//            System.out.println("Insert Error");
             return false;
         }
     }
 
     /**
      * Adds new line to file
-     * @param (String) Line to add
+     *
+     * @param add (String) Line to add
      * @return (boolean) Returns true if writing new line was successful, false if not
      */
     public boolean writeNewLine(String add)
     {
-        try{
+        try {
             FileWriter newLineWriter = new FileWriter(file, true);
-            newLineWriter.append("\n"+add);
+            newLineWriter.append("\n" + add);
             newLineWriter.flush();
             newLineWriter.close();
             return true;
-        } catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error- Could not write new line");
             return false;
         }
@@ -118,25 +132,27 @@ public class FileOperator
 
     /**
      * Removes line in file
-     * @param (int) Row to remove
+     *
+     * @param row (int) Row to remove
      * @return (boolean) Returns true if removing line was successful, false if not
      */
     public boolean removeLine(int row)
     {
-        try{
+        try {
             ArrayList<String> contents = readToList();
             contents.remove(row);
             clearFile();
             FileWriter appendFileWriter = new FileWriter(file, true); //Writes each row to file
-            for(int i=0; i<contents.size(); i++){
+            for (int i = 0; i < contents.size(); i++) {
                 appendFileWriter.append(contents.get(i));
-                if(i!=contents.size()-1) {appendFileWriter.append("\n");} //Don't want empty row at EOF 
+                if (i != contents.size() - 1) {
+                    appendFileWriter.append("\n");
+                } //Don't want empty row at EOF
             }
             appendFileWriter.flush();
             appendFileWriter.close();
             return true;
-        } catch(Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Error- Could not remove line");
             return false;
         }
@@ -144,29 +160,41 @@ public class FileOperator
 
     /**
      * Clears contents of file
+     *
      * @return (boolean) Returns true if clearing file was successful, false if not
      */
     public boolean clearFile()
     {
-        try{
+        try {
             FileWriter clearFileWriter = new FileWriter(file, false);
             clearFileWriter.append("");
             clearFileWriter.flush();
             clearFileWriter.close();
             return true;
-        } catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error- Could not clear file");
             return false;
         }
     }
+
     /**
      * Prints file
      */
     public void printFile()
     {
         ArrayList<String> contents = readToList();
-        for(int i=0; i<contents.size(); i++){
+        for (int i = 0; i < contents.size(); i++) {
             System.out.println(contents.get(i));
         }
+    }
+
+    public boolean isAvailable()
+    {
+        return available;
+    }
+
+    public Exception getException()
+    {
+        return exception;
     }
 }
---
+++ test/FileOperatorTest.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileOperatorTest
+{
+    private FileOperator fileOps;
+
+    @BeforeEach
+    void setUp()
+    {
+
+        this.fileOps = new FileOperator(getClass().getResource("map.csv").getPath());
+        if (!this.fileOps.isAvailable()) {
+            System.out.println("The file is not available for reading");
+        }
+    }
+
+    @Test
+    void testAReadToList()
+    {
+        ArrayList<String> listOfStrings = fileOps.readToList();
+        assertEquals(listOfStrings.size(), 4);
+        assertEquals(listOfStrings.get(0), "0,1,2,3");
+    }
+
+    @Test
+    void testBReadLine()
+    {
+        String[] line;
+        line = fileOps.readLine(0);
+
+        assertEquals(line.length, 4);
+        assertEquals(line[0], "0");
+
+        line = fileOps.readLine(7);
+        assertNull(line);
+    }
+
+    @Test
+    void testCReadElement()
+    {
+        assertEquals(fileOps.readElement(0, 0), "0");
+        assertEquals(fileOps.readElement(3, 3), "1");
+        assertNull(fileOps.readElement(8, 8));
+        assertNull(fileOps.readElement(8, 4));
+        assertNull(fileOps.readElement(4, 8));
+    }
+
+    @Test
+    void testDInsert()
+    {
+        assertTrue(this.fileOps.insert("8", 0, 0));
+        assertFalse(this.fileOps.insert("8", 8, 8));
+        assertEquals(this.fileOps.readElement(0, 0), "8");
+        assertTrue(this.fileOps.insert("0", 0, 0));
+    }
+
+    @Test
+    void testEWriteNewLine()
+    {
+        assertTrue(fileOps.writeNewLine("5,4,3,2"));
+    }
+
+    @Test
+    void testFRemoveLine()
+    {
+        assertTrue(fileOps.removeLine(4));
+        assertFalse(fileOps.removeLine(9));
+    }
+}
