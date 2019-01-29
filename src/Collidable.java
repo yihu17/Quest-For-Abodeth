f5443ae8aa8f35eca36b7d6954108f189a16fc66
Ethan Cotterell <e.cotterell@lancaster.ac.uk>
2019-01-29 22:52:57 +0000

    Javadoc'ed more interfaces

--- src/Collidable.java
+++ src/Collidable.java
@@ -3,11 +3,28 @@
  */
 public interface Collidable
 {
+    /**
+     * Returns the top left X coordinate of the object
+     *
+     * @return (float) X coordinate
+     */
     float getX();
 
+    /**
+     * Returns the top left Y coordinate of the object
+     * @return (float) Y coordinate
+     */
     float getY();
 
+    /**
+     * Returns the height of the object
+     * @return (float) Object height
+     */
     float getHeight();
 
+    /**
+     * Returns the width of the object
+     * @return (float) Object width
+     */
     float getWidth();
 }
--- src/Powerup.java
+++ src/Powerup.java
@@ -1,10 +1,30 @@
 import org.jsfml.graphics.Drawable;
 
+/**
+ * All powerups implement this so that their special effect can be applied
+ * to the player
+ */
 public interface Powerup extends Drawable, Collidable
 {
+    /**
+     * Applies this powerups special effect to the given
+     * player object
+     *
+     * @param p (Player) Current player
+     */
     void applyBuff(Player p);
 
+    /**
+     * Removes this powerups special effect to the given
+     * player object
+     * @param p (Player) Current player
+     */
     void removeBuff(Player p);
 
+    /**
+     * Returns the Image object attached to this powerup so that it can be
+     * drawn to the screen
+     * @return (Image) Powerup image
+     */
     Image getImage();
 }
