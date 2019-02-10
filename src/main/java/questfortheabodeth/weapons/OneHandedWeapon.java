7b801456428fd1aab42f85dcd547854370831088
Ethan <e.cotterell@lancaster.ac.uk>
2019-02-10 11:09:47 +0000

    Added two new weapon classes so we can type the weapons picked up and handled by the player

---
+++ src/main/java/questfortheabodeth/weapons/OneHandedWeapon.java
@@ -0,0 +1,7 @@
+package main.java.questfortheabodeth.weapons;
+
+public class OneHandedWeapon extends Gun {
+    public OneHandedWeapon(String name, int bulletsPerShot, int maxAmmo, int fireRate) {
+        super(name, bulletsPerShot, maxAmmo, fireRate);
+    }
+}
---
+++ src/main/java/questfortheabodeth/weapons/TwoHandedWeapon.java
@@ -0,0 +1,7 @@
+package main.java.questfortheabodeth.weapons;
+
+public class TwoHandedWeapon extends Gun {
+    public TwoHandedWeapon(String name, int bulletsPerShot, int maxAmmo, int fireRate) {
+        super(name, bulletsPerShot, maxAmmo, fireRate);
+    }
+}
