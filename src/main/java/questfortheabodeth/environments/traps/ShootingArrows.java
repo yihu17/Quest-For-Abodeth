dfe8a125b592a148afa0e0d30cd96a3b666ecf17
edwardinio <eddycorke@gmail.com>
2019-02-15 16:12:18 +0000

    Changed files in res and added: TrapZone interface and ShootingArrow class

---
+++ src/main/java/questfortheabodeth/environments/traps/ShootingArrows.java
@@ -0,0 +1,28 @@
+package main.java.questfortheabodeth.environments.traps;
+
+import main.java.questfortheabodeth.environments.CollidableEnvironment;
+import org.jsfml.graphics.FloatRect;
+
+public class ShootingArrows extends CollidableEnvironment implements TrapZone {
+
+    public ShootingArrows(int xPos, int yPos, String imageFilePath) {
+        super(xPos, yPos, imageFilePath);
+    }
+
+    @Override
+    public void effect() {
+        //if
+    }
+
+    @Override
+    public FloatRect getGlobalBounds()
+    {
+        //create new floatRect (upper right to lower left)
+        return super.getGlobalBounds(); //swap with new floatrect
+    }
+
+    @Override
+    public void trigger() {
+        System.out.println("Shooting arrow trap triggered!");
+    }
+}
